52150b0bb591eb1ebcf8e0e83ed2ea8653441d25
archlichxw@gmail.com <archlichxw@gmail.com>
2015-08-25 20:15:00 +0800

    代码拆分 Signed-off-by: archlichxw@gmail.com <archlichxw@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,159 +1,15 @@
-
-/*
- * grunt-ttf2base64-and-otherfonts
- * https://github.com/leon776/grunt-ttf2base64-and-otherfonts
- *
- * Copyright (c) 2015 xiaoweili
- * Licensed under the MIT license.
+/**
+ * Created by xiaoweili on 2015/8/25.
  */
-
-'use strict';
+var CssOperater = require('./css_operater');
 var path = require('path'),
     fs = require('fs'),
     md5 = require('md5'),
-    gui = require('nw.gui'),
     moment = require('moment'),
     template = require('art-template'),
     msgTepl, render, msgTeplCss, renderCss;
-    //shell = gui.Shell;
-    global.$ = $;
-    global.data = {};
-    global.cssData = {};
-
-var CssOperater = require('./js/css_operater');
-//debug
-for(var module in global.require.cache){
-    if(global.require.cache.hasOwnProperty(module)){
-        delete global.require.cache[module];
-    }
-}
-//debug
-
-//工具函数
-Array.prototype.unique = function(){
-    if(this.length === 0) {
-        return [];
-    }
-    this.sort();
-    var re = [this[0]];
-    for(var i = 1; i < this.length; i++)
-    {
-        if( this[i] !== re[re.length-1])
-        {
-            re.push(this[i]);
-        }
-    }
-    return re;
-};
-var preventDefault = function() {
-    $(document).on({
-        dragleave:function(e){    //拖离
-            e.preventDefault();
-        },
-        drop:function(e){  //拖后放
-            e.preventDefault();
-        },
-        dragenter:function(e){    //拖进
-            e.preventDefault();
-        },
-        dragover:function(e){    //拖来拖去
-            e.preventDefault();
-        }
-    });
-},
-domDragOn = function(obj) {
-    $(obj).addClass('on');
-    $(obj).children('.label-area').hide();
-    $(obj).children('p').show();
-},
-domDragEnd = function(obj) {
-    $(obj).removeClass('on');
-    $(obj).children('p').hide();
-    $(obj).children('.label-area').show();
-},
-updateTestCase = function(src) {
-    console.log(src)
-    app.createTestJson(src);
-},
-showLog = function(id) {
-    var win = gui.Window.open('log.html', {
-        position: 'center',
-        width: 800,
-        height: 600
-    });
-    win.on('document-end', function () {
-        win.window.document.getElementById('log').value = global.data[id].data;
-    });
-},
-showLogCss = function(id) {
-    var win = gui.Window.open('log.html', {
-        position: 'center',
-        width: 800,
-        height: 600
-    }), str = '';
-    win.on('document-end', function () {
-        var _data = global.cssData[id].unique();
-        for(var i = 0; i < _data.length; i++) {
-            str += 'html文件路径：' + _data[i].path + '\n\n';
-            str += 'css文本：' + _data[i].cssText + '\n\n';
-            str += 'dom节点：' + _data[i].html + '\n';
-        }
-        win.window.document.getElementById('log').value = str;
-    });
-};
 
 
-var FileOperater = {
-    fileMapping : function(fpath) {
-        var tmp = [];
-        var files = fs.readdirSync(fpath);
-        files.forEach(function(file){
-            var tempPath = path.join(fpath, file);
-            var stats = fs.statSync(tempPath);
-            if(stats.isDirectory()){
-                tmp.push(path.basename(tempPath));
-            }
-        });
-        return tmp;
-    },
-    scanFolder : function (fpath){
-        var fileList = [],
-            folderList = [],
-            files,
-            walk = function(fpath, fileList, folderList){
-                files = fs.readdirSync(fpath);
-                files.forEach(function(item) {
-                    var tmpPath = path.join(fpath, item),
-                        stats = fs.statSync(tmpPath);
-                    if (stats.isDirectory()) {
-                        walk(tmpPath, fileList, folderList);
-                        folderList.push(tmpPath);
-                    } else {
-                        fileList.push(tmpPath);
-                    }
-                });
-            };
-
-        walk(fpath, fileList, folderList);
-        console.log('扫描' + fpath +'成功');
-        return fileList;
-    },
-    deleteFolderRecursive : function(fpath) {
-        var files = [];
-        if( fs.existsSync(fpath) ) {
-            files = fs.readdirSync(fpath);
-            files.forEach(function(file, index){
-                var curPath = path.join(fpath, file);
-                if(fs.statSync(curPath).isDirectory()) { // recurse
-                    deleteFolderRecursive(curPath);
-                } else { // delete file
-                    fs.unlinkSync(curPath);
-                }
-            });
-            fs.rmdirSync(fpath);
-        }
-    }
-};
 
 var App = (function() {
     var App = function(option) {
@@ -175,12 +31,11 @@ var App = (function() {
         this.dragBox.bind('dragleave', function(e) {
             domDragEnd(this);
         });
-        $('#nav li').click(function() {
-            $('#nav li').removeClass('active');$(this).addClass('active');
-            $('.controls').hide(); $('.' + $(this).attr('data-for')).show();
-        });
         $('#createVersion').click(function() {
             self.createTestJson();
+        });
+        $('#delVersion').click(function() {
+            self.delTestVer();
         });
         $('#startTest').click(function() {
             self.startTest();
@@ -213,7 +68,7 @@ var App = (function() {
                 fs.mkdirSync(self.dest + '/.testdata');
             }
             self.htmlfile = [];
-            self.loadTestData();
+            self.loadTestVer();
             self.readFolder(fileList[0].path);
         });
         $('#filePathDialog').change(function() {
@@ -223,11 +78,10 @@ var App = (function() {
                 fs.mkdirSync(self.dest + '/.testdata');
             }
             self.htmlfile = [];
-            self.loadTestData();
+            self.loadTestVer();
             self.readFolder(this.value);
         });
     };
-    //清理css功能start
     App.prototype.clearUselessCss = function(cssPath) {
         if(this.useLessCss.length <= 0) {
             this.tip('清先扫描css', 'error');
@@ -245,40 +99,36 @@ var App = (function() {
             cssText = fs.readFileSync(cssPath, 'utf-8');
         self.cssSelecter = self.getSelecter(cssText);
         self.tip('css扫描中，请稍后', '');
-
         _scan();
-
         function _scan() {
             for(var i = self.curPage; i < self.htmlfile.length; i++) {
                 self.frame.src = self.htmlfile[i].path;
                 self.frame.onload = function() {
                     for (var j = 0; j < self.cssSelecter.length; j++) {
                         try{
-                            var _selecter = _filterCssSelecter(self.cssSelecter[j]),
-                                _dom = this.contentWindow.document.querySelectorAll(_selecter),
+                            var _dom = this.contentWindow.document.querySelectorAll(self.cssSelecter[j].split(':')[0]),
                                 _l = _dom.length,
                                 _domHtml = '',
                                 _key = md5(self.cssSelecter[j]);
                             hit[_key] = hit[_key] ? hit[_key] + _l : _l;
-
                             if(_l > 0) {
                                 useFulCss[_key] = useFulCss[_key] ? useFulCss[_key] : [];
                                 for(var k = 0; k < _l; k++) {
                                     _domHtml +=
-                                      '\n================第'+ k +'个被命中的节点开始===============\n'
-                                    + _dom[k].outerHTML
-                                    + '\n================第'+ k +'个被命中的节点结束===============\n'
+                                        '\n================第'+ k +'个被命中的节点开始===============\n'
+                                        + _dom[k].outerHTML
+                                        + '\n================第'+ k +'个被命中的节点结束===============\n'
                                 }
                                 useFulCss[_key].push({
                                     path: self.htmlfile[i].path,
                                     html: _domHtml,
                                     selecter: self.cssSelecter[j],
                                     cssText:
-                                          '\n******************************************************\n'
-                                        + self.getCssText(cssText, self.cssSelecter[j])
-                                        + '\n******************************************************'
+                                    '\n******************************************************\n'
+                                    + self.getCssText(cssText, self.cssSelecter[j])
+                                    + '\n******************************************************'
                                 });
-                                _useFulCssSelecter.push(self.cssSelecter[j]);
+                                _useFulCssSelecter.push(cssText, self.cssSelecter[j]);
                                 //break;
                             }
                         } catch(e) {
@@ -317,15 +167,7 @@ var App = (function() {
             self.useLessCss = useLessCss;
             $('#outPutMsg').html('').append(renderCss({"msg" : msg}));
         }
-        function _filterCssSelecter(selecter) {
-            var jsSelecter = $('#filterJsClass').val().split(',');
-            if(jsSelecter.length !== 0) {
-                for(var i = 0; i < jsSelecter.length; i++) {
-                    selecter = selecter.replace(new RegExp('.' + jsSelecter[i], 'g'), '');
-                }
-            }
-            return selecter.split(':')[0];
-        }
+
         function _cj(a, b) { // 差集
             return $.merge($.grep(a, function(i) {
                     return $.inArray(i, b) == -1;
@@ -335,56 +177,44 @@ var App = (function() {
             );
         }
     };
-    //清理css功能end
-    App.prototype.createTestJson = function(fileSrc) {
+
+    App.prototype.createTestJson = function() {
         if(this.htmlfile.length === 0) {
             this.tip('请先选择html目录', 'alert-error');
             return;
         }
-        var self = this,
-            ver = self.dest + '/.testdata/';
+        var self = this, ver = moment().format("MM月DD日hh时mm分ss秒");
+        ver = self.dest + '/.testdata/' + ver;
+        fs.mkdirSync(ver);
         self.tip('测试数据创建中，请稍后', '');
-        $(self.frame).css({
-            width: $('#iframeWidth').val(),
-            height: $('#iframeHeight').val()
-        });
-
-        _create(fileSrc);
-        function _create(fileSrc) {
-            self.frame.src = fileSrc ? fileSrc : self.htmlfile[self.curPage].path;
-            self.frame.onload = function() {
-                var frame = this;
-                window.setTimeout(function() {
-                    var nodes = frame.contentWindow.document.getElementsByTagName('*');
+        _create();
+        function _create() {
+            for(var i = self.curPage; i < self.htmlfile.length; i++) {
+                self.frame.src = self.htmlfile[i].path;
+                self.frame.onload = function() {
+                    var nodes = this.contentWindow.document.getElementsByTagName('*');
                     var json = JSON.stringify(self.getStyleJson(nodes));
-                    if(!fileSrc){
-                        fs.writeFile(ver + '/' + self.htmlfile[self.curPage].name + '.json', json, 'utf-8');
-                        self.curPage++;
-                    } else {
-                        fs.writeFile(ver + '/' + path.basename(fileSrc) + '.json', json, 'utf-8');
-                    }
+                    fs.writeFile(ver + '/' + self.htmlfile[i].name + '.json', json, 'utf-8');
+                    self.curPage++;
                     self._showProcess();
-                    if(self.curPage == self.htmlfile.length || fileSrc) {
+                    if(self.curPage == self.htmlfile.length) {
                         self.tip('版本生成成功', 'alert-success');
-                        self.loadTestData();
+                        self.loadTestVer();
                         self.curPage = 0;
                     } else {
-                        if(!fileSrc) _create();
+                        _create();
                     }
-                }, 300)
-            };
+                };
+                break;
+            }
         }
     };
     App.prototype.startTest = function() {
         var self = this;
-        var testDataPath = this.dest + '/.testdata/',
-            testData;
+        var testDataPath = this.dest + '/.testdata' + '/' + $('#htmlVersion')[0].value,
+            testData, _msg;
         this.clearMsg();
         self.tip('测试中，请稍后', '');
-        $(self.frame).css({
-            width: $('#iframeWidth').val(),
-            height: $('#iframeHeight').val()
-        });
         _contrast();
         function _contrast() {
             for(var i = self.curPage; i < self.htmlfile.length; i++) {
@@ -393,31 +223,45 @@ var App = (function() {
                     var tmp = i;
                     self.frame.src = self.htmlfile[i].path;
                     self.frame.onload = function() {
-                        var frame = this;
-                        window.setTimeout(function() {
-                            var nodes = frame.contentWindow.document.getElementsByTagName('*');
-                            self.curPage++;
-                            $('#outPutMsg').append(render({"msg" : self.contrastPage(nodes, testData, self.htmlfile[tmp].path)}));
-                            self._showProcess();
-                            if(self.curPage == self.htmlfile.length) {
-                                self.tip('测试完毕', 'alert-success');
-                                self.curPage = 0;
-                            } else {
-                                _contrast();
-                            }
-                        }, 300);
+                        var nodes = this.contentWindow.document.getElementsByTagName('*');
+                        self.curPage++;
+                        //var t = self.contrastPage(nodes, testData, self.htmlfile[tmp].path);
+                        console.log(123)
+                        //$('#outPutMsg').append(render({"msg" : 123}));
+                        self._showProcess();
+                        if(self.curPage == self.htmlfile.length) {
+                            self.tip('测试完毕', 'alert-success');
+                            self.curPage = 0;
+                        } else {
+                            _contrast();
+                        }
                     };
                 })();
-
                 break;
             }
         }
     };
-
-    App.prototype.loadTestData = function() {
-        this.testData = FileOperater.fileMapping(this.dest + '/.testdata');
+    App.prototype.loadTestVer = function() {
+        var tmp = '';
+        this.testData = fileMapping(this.dest + '/.testdata');
+        this.testData.forEach(function(name) {
+            tmp += '<option value="'+name+'">'+name+'</option>';
+        });
+        $('#htmlVersion').html(tmp);
+    };
+    App.prototype.delTestVer = function() {
+        if(!$('#htmlVersion')[0].value) {
+            this.tip('已经木有版本数据了', 'alert-error');
+            return;
+        }
+        deleteFolderRecursive(this.dest + '/.testdata' + '/' + $('#htmlVersion')[0].value);
+        this.tip('版本'+ $('#htmlVersion')[0].value +'删除成功', 'alert-success');
+        this.loadTestVer();
     };
 
+    App.prototype.clearMsg = function() {
+        $('#outPutMsg').html('');
+    };
 
     App.prototype._showProcess = function() {
         var pro = Math.round((this.curPage) * 100 / this.htmlfile.length);
@@ -431,7 +275,7 @@ var App = (function() {
 
 
     App.prototype.readFolder = function(fpath) {
-        var files = FileOperater.scanFolder(fpath), msg = [];
+        var files = scanFolder(fpath), msg = [];
         for(var name in files) {
             if(path.extname(files[name]) === '.html') {
                 this.htmlfile.push({
@@ -440,8 +284,7 @@ var App = (function() {
                 });
                 msg.push({
                     "txt": '读取' + path.basename(files[name]) + '成功',
-                    "type": 0,
-                    "id": files[name].replace(/\\/g, '\\\\')
+                    "type": 0
                 });
             }
         }
@@ -451,9 +294,6 @@ var App = (function() {
 
     App.prototype.tip = function(txt, cls) {
         $('#alert').html(txt)[0].className = 'alert ' + cls;
-    };
-    App.prototype.clearMsg = function() {
-        $('#outPutMsg').html('');
     };
 
     App.prototype.init = function() {
@@ -468,16 +308,9 @@ var App = (function() {
         render = template.compile(msgTepl);
         renderCss = template.compile(msgTeplCss);
         this.bindEvent();
-        this.loadTestData();
+        this.loadTestVer();
         this.readFolder(this.dest);
     };
     return App;
 })();
-
-$(document).ready(function() {
-    var app = new App({
-        dragBox : 'dragUpload'
-    });
-    app.init();
-    window.app = app;
-});
+module.exports = App;
--- js/css_operater.js
+++ js/css_operater.js
@@ -1,40 +1,46 @@
+
 /**
  * Created by xiaoweili on 2015/8/17.
  */
+var CleanCSS = require('clean-css'),
+    md5 = require('md5');
+
 //css处理类
 var CssOperater = (function() {
     var Operater = function() {};
     Operater.prototype.getStyleJson = function (nodes) {
-        var tmp = {};
+        var res = {};
         for(var i = 0; i < nodes.length; i++) {
-            tmp[md5(i + nodes[i].tagName)] = this.getStyle(nodes[i]);
+            res[md5(i + nodes[i].tagName)] = this._getStyle(nodes[i]);
         }
-        return tmp;
+        return res;
     };
-    Operater.prototype.getStyle = function (obj) {
-        var cupStyle = getComputedStyle(obj);
-        var tmp = {};
+    //获取dom节点计算样式
+    Operater.prototype._getStyle = function (obj) {
+        var cupStyle = window.getComputedStyle(obj);
+        var res = {};
         for(var name in cupStyle) {
             if(!isNaN(parseInt(name))) {
                 continue;
             }
             if( cupStyle[name] && typeof(cupStyle[name]) === 'string' && name !== 'cssText' ) {
-                tmp[name] = cupStyle[name];
+                res[name] = cupStyle[name];
             }
         }
-        return tmp;
+        return res;
     };
+    //删除无用样式
     Operater.prototype.delUselsessSelecter = function (str, selecter) {
         var _selecter, regComma, regStyle, length = selecter.length;
+        str = str.replace(/\s+[>]\s+/g, '>');
         for (var i = 0; i < length; i++) {
-            _selecter = selecter[i].replace('(', '\\(').replace(')', '\\)')
-                .replace('^', '\\^')
-                .replace(']', '\\]')
-                .replace('[', '\\[');
-            regComma  = new RegExp('([,\\t\\n\\s\\/]+)' + _selecter + '\\s*,', "g");
-            regStyle  = new RegExp('(}[\\t\\n\\s\\/]*)' + _selecter + '\\s*{([^}]*)}', "g");
+            _selecter = selecter[i].replace(/[\(\)\^\[\]\*\\\/]/g, '\\$1').
+                                    replace(/\s+/g, '\\s');
+            regComma  = new RegExp('([},\\t\\n\\s\\/]+)' + _selecter + '\\s*,', "g");
+            regStyle  = new RegExp('([},][\\t\\n\\s\\/]*|\\*\\/[\\t\\n\\s\\/]*)' + _selecter + '\\s*{([^}]*)}', "g");
+
             str       = str.replace(regComma, '$1').replace(regStyle, '$1');
-        };
+        }
         return str;
     };
     //获取所有的css选择器
@@ -42,31 +48,46 @@ var CssOperater = (function() {
         var minified = "}" + new CleanCSS().minify(str).styles,
             selecter = [],
             reg = new RegExp("}([^{}%@]*){","g"),
-            mediaFilter = new RegExp(/@media[^{}]*{/g);
+            mediaFilter = new RegExp(/@media[^{}]*{/g),
+            charsetFilter = new RegExp(/@charset[^{}]*;/g);
 
-        minified = minified.replace(mediaFilter, "$&}");
+        minified = minified.replace(mediaFilter, "$&}").replace(charsetFilter, "$&}");
         var tmparray = minified.match(reg);
+
         for (var i = 0; i < tmparray.length; i++) {
             var tmpStr = tmparray[i].replace('}','').replace('{','');
             selecter.push(tmpStr);
         }
         return selecter.join(',').split(',');
     };
-    Operater.prototype.contrastPage = function (nodes, testData, pageName) {
-        var flag = false, msg = [], str = '', pid = md5(pageName);
-        for(var i = 0; i < nodes.length; i++) {
-            var node = this.getStyle(nodes[i]),
+    //获取选择器内容
+    Operater.prototype.getCssText = function(str, selecter) {
+        var _selecter = selecter.replace(/[\(\)\^\[\]\>\<]/g, '\\$1');
+        var reg  = new RegExp(_selecter + '[^}]*?}', "g");
+        return str.match(reg)[0];
+    };
+    Operater.prototype.contrastPage = function (nodes, testData, pageName, callback) {
+        var flag = false,
+            msg = [],
+            str = '',
+            pid = md5(pageName),
+            _hasChangedNodes = [];
+        for(var i = nodes.length - 1; i >= 0; i--) {
+            var node = this._getStyle(nodes[i]),
                 testDataTmp = testData[md5(i + nodes[i].tagName)];
-            //console.log(testDataTmp)
             if(!testDataTmp) {
-                //console.log(nodes[i] + pageName)
+
             }
             else if( JSON.stringify(node) !== JSON.stringify(testDataTmp) ) {
                 if(!flag) {
                     str += "网页"+ pageName + "有变化\n";
                 }
                 flag = flag ? flag : true;
+                if(_hasChangedChild(nodes[i].outerHTML)) {
+                    continue;//有子节点发生改变的节点就不统计了
+                }
                 str += "\n元素" + nodes[i].outerHTML + '发生了改变\n';
+                _hasChangedNodes.push(nodes[i].outerHTML);
                 for (var j in testDataTmp) {
                     if(!(testDataTmp[j] === node[j])) {
                         str += j + '的值由：' + testDataTmp[j] + '变为了：' + node[j] + '\n';
@@ -88,9 +109,18 @@ var CssOperater = (function() {
             });
         }
         global.data[pid] = {"data" : str};
-        $('#outPutMsg').append(render({"msg" : msg}));
+        return msg;
+        //判断是否有子节点发生改变
+        function _hasChangedChild(str) {
+            //console.log(str)
+            for(var i = 0; i < _hasChangedNodes.length; i++) {
+                if( str.indexOf(_hasChangedNodes[i]) > 0 ) {
+                    return true;
+                }
+            }
+            return false;
+        }
     };
     return Operater;
 })();
-
-
+module.exports = CssOperater;
